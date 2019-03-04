967cafb7bc58b400d9ff2017c38ee6e451fc927e
rhmerx <rhmerx@gmail.com>
2019-03-03 19:57:17 -0500

    changes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require("express");
 var app = express();
 
 var serverPort = process.env.VCAP_APP_PORT || 3000;
-var serverHost = process.env.VCAP_APP_HOST || 'localhost';
+// var serverHost = process.env.VCAP_APP_HOST || 'localhost';
 
 app.get('/', function (req, res) {
-  res.send('Hello Coursera');
+  res.send('Hello World, I made changes to the app.');
 });
 
-var server = app.listen(serverPort, serverHost, function () {
+var server = app.listen(serverPort, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
