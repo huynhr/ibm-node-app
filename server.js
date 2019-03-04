4c08a21b514cd0c237fe364e0a251aca34cc62e7
Raymond Huynh <huynh.r.k@gmail.com>
2019-03-03 19:31:12 -0500

    changes

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+var serverPort = process.env.VCAP_APP_PORT || 3000;
+var serverHost = process.env.VCAP_APP_HOST || 'localhost';
+
+app.get('/', function (req, res) {
+  res.send('Hello Coursera');
+});
+
+var server = app.listen(serverPort, serverHost, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
+});
