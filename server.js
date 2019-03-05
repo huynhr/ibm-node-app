67beeea9a09fcb1246ff38e6e95f0ab824ea40c3
rhmerx <rhmerx@gmail.com>
2019-03-05 08:32:15 -0500

    Add ibmiotf library and connections

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+var iotf = require('ibmiotf');
+var appConfig;
 
-var serverPort = process.env.VCAP_APP_PORT || 3000;
-// var serverHost = process.env.VCAP_APP_HOST || 'localhost';
+var serverPort = process.env.PORT || 3000;
 
+// parse out the VCAP_SERVICES environment variable to retrieve the credentials for the IoTF service.
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  appConfig = {
+    'org': env["iotf-service"][0].credentials.org,
+    'id': 'bi-nodeserver',
+    'auth-key': env["iotf-service"][0].credentials.apiKey,
+    'auth-token': env["iotf-service"][0].credentials.apiToken
+  }
+} else {
+  appConfig = require('./application.json');
+}
+
+var responseString = 'Hello Coursera';
+
+var appClient = new iotf.IotfApplication(appConfig);
 app.get('/', function (req, res) {
-  res.send('Hello World, I made changes to the app.');
+  res.send(responseString);
 });
 
 var server = app.listen(serverPort, function () {
-  var host = server.address().address;
   var port = server.address().port;
-  console.log('Listening at http://%s:%s', host, port);
+  console.log('Listening on port : %s', port);
+  // connect to the IoTF service
+  appClient.connect();
+
+  // subscribe for device events
+  appClient.on('connect', function () {
+    appClient.subscribeToDeviceEvents();
+  });
+  
+  // handle device events when they arrive
+  appClient.on('deviceEvent', function (deviceType, deviceId, eventType, format, payload) {
+    responseString = "Device event at " + new Date().toString() + " from " + deviceType +
+      ":" + deviceId + "; event = " + eventType + ", payload = " + payload;
+  });
 });
